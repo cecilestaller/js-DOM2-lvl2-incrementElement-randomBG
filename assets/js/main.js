e6214208f11f59a86db6d150c3f8718cb8ca68fd
cecilestaller <c.staller96@gmail.com>
2023-11-14 16:06:08 +0100

    DOM2 Aufgaben divCounter randomBackground

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+console.log("%c DOM-Elements Level2_7", "color:tomato");
+// AUFGABE: - Schreibe eine Funktion, die beim Anklicken des Buttons ein Element mit aufsteigenden Nummern erstellt.
+// - Nutze: querySelector(), addEventListener(), let counter = 0;, createElement(), textContent(), appendChild() und classList.
+
+const clickButton = document.body.querySelector('.button1');
+const umwickelnDiv = document.body.querySelector('.umwickeln');
+
+let counter = 0;
+
+clickButton.addEventListener('click', () => {
+    const div = document.createElement('div');
+    div.textContent = counter;
+    if (counter%10 === 0){
+        div.classList.add('weiss', 'rechteck');
+        umwickelnDiv.appendChild(div);
+    } else {
+        div.classList.add('rechteck');
+        umwickelnDiv.appendChild(div);
+    }
+    counter++;
+})
+
+
+console.log("%c DOM-Elements Level2_11", "color:tomato");
+
+const bgButton = document.body.querySelector('.button2');
+
+bgButton.addEventListener('click', () => {
+    const red = Math. floor(Math. random() * 256);
+    const green = Math. floor(Math. random() * 256);
+    const blue = Math. floor(Math. random() * 256);
+    document.body.style.backgroundColor = "rgb(" + red + ", " + green + ", " + blue + ")";
+});
